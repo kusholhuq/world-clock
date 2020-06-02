89f4ccf240e9e5c2cf5d94ba34b9d67e495864b3
Mohsinul Huq <kusholhuq@gmail.com>
2020-06-01 23:55:36 -0700

    removed excess code and comments from main.js, still need to clean up style.css

--- server/public/main.js
+++ server/public/main.js
@@ -89,63 +89,10 @@ setInterval(showTime, 1000);
 
 var body = document.body;
 var box = document.querySelectorAll(".box");
-// var dark = document.querySelector("#dark");
-// var water = document.querySelector("#water");
-// var lights = document.querySelector("#lights");
-// var rain = document.querySelector("#rain");
 var input = document.getElementById("input");
-
-// dark.addEventListener("click", handleDark);
-// water.addEventListener("click", handleWater);
-// lights.addEventListener("click", handleLights);
-// rain.addEventListener("click", handleRain);
-
-function handleDark(event) {
-  body.setAttribute("class", "dark");
-  input.classList.add("white");
-  input.classList.remove("black");
-  for (var i = 0; i < box.length; i++) {
-    box[i].classList.remove("pink");
-    box[i].classList.add("grey");
-  }
-}
-function handleWater(event) {
-  body.setAttribute("class", "bright");
-  input.classList.add("black");
-  input.classList.remove("white");
-  for (var i = 0; i < box.length; i++) {
-    box[i].classList.add("pink");
-    box[i].classList.remove("grey");
-  }
-}
-function handleLights(event) {
-  body.setAttribute("class", "lights");
-  input.classList.add("white");
-  input.classList.remove("black");
-  for (var i = 0; i < box.length; i++) {
-    box[i].classList.add("pink");
-    box[i].classList.remove("grey");
-  }
-}
-function handleRain(event) {
-  body.setAttribute("class", "rain");
-  input.classList.add("black");
-  input.classList.remove("white");
-  for (var i = 0; i < box.length; i++) {
-    box[i].classList.add("grey");
-    box[i].classList.remove("pink");
-  }
-}
 
 const backgrounds = ['stars','haze', 'leaf','lights', 'meteor', 'sun']
 let currentIndex = 0;
-
-function checkFont(){
-  //if current index equals this this or this
-  //make font colors white or black in response
-  //and make squares diff color in response
-  //can do a switch statement
-}
 
 function applyBackground() {
   body.setAttribute('class', backgrounds[currentIndex]);
@@ -159,7 +106,6 @@ function cycleBackgroundForward(event){
   if(currentIndex<0){
     currentIndex=5;
   }
-  console.log(currentIndex)
   applyBackground();
 }
 
@@ -171,7 +117,6 @@ function cycleBackgroundBack(event){
   if (currentIndex < 0) {
     currentIndex = 5;
   }
-  console.log(currentIndex)
   applyBackground();
 }
 
